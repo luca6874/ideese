4c4eac63a585836fd63f2907cb8bf3d100f6b3fe
luca6874 <lreinaga_24@alu.uabcs.mx>
2026-04-16 00:16:47 -0600

    Recupero mi version de Memorice

--- src/programacion/Memorice.java
+++ src/programacion/Memorice.java
@@ -6,13 +6,8 @@ import java.awt.Color;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Dimension;
 
 public class Memorice extends JFrame {
 
@@ -43,43 +38,55 @@ public class Memorice extends JFrame {
         panel_1.setBackground(new Color(237, 243, 125));
         getContentPane().add(panel_1, BorderLayout.SOUTH);
         
-        JButton btnNewButton_1 = new JButton("Eri mi loba");
+        JButton btnNewButton_1 = new JButton("Reiniciar");
         panel_1.add(btnNewButton_1);
-
         
-        JButton btnNewButton = new JButton("SShlk");
+        JButton btnNewButton = new JButton("Pausa");
         panel_1.add(btnNewButton);
         
         JPanel panel_2 = new JPanel();
         panel_2.setBackground(new Color(137, 246, 216));
         getContentPane().add(panel_2, BorderLayout.CENTER);
         panel_2.setLayout(new GridLayout(3, 4, 5, 5));
-
-        ImageIcon iconOriginal = new ImageIcon(getClass().getResource("/imagen/yo.png"));
-        Image imagen = iconOriginal.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-        ImageIcon icon = new ImageIcon(imagen);
         
+        JButton btnNewButton_2 = new JButton();
+        panel_2.add(btnNewButton_2);
         
-
-        for (int i = 0; i < 12; i++) {
-            JButton btn = new JButton();
-            btn.setIcon(icon);
-            panel_2.add(btn);
-            btn.setPreferredSize(new Dimension(100, 100));
-
-            btn.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("Botón presionado");
-                }
-            });
-        }
-
-            
+        JButton btnNewButton_3 = new JButton();
+        panel_2.add(btnNewButton_3);
+        
+        JButton btnNewButton_4 = new JButton();
+        panel_2.add(btnNewButton_4);
+        
+        JButton btnNewButton_8 = new JButton();
+        panel_2.add(btnNewButton_8);
+        
+        JButton btnNewButton_6 = new JButton();
+        panel_2.add(btnNewButton_6);
+        
+        JButton btnNewButton_11 = new JButton();
+        panel_2.add(btnNewButton_11);
+        
+        JButton btnNewButton_5 = new JButton();
+        panel_2.add(btnNewButton_5);
+        
+        JButton btnNewButton_7 = new JButton();
+        panel_2.add(btnNewButton_7);
+        
+        JButton btnNewButton_9 = new JButton();
+        panel_2.add(btnNewButton_9);
+        
+        JButton btnNewButton_10 = new JButton();
+        panel_2.add(btnNewButton_10);
+        
+        JButton btnNewButton_12 = new JButton();
+        panel_2.add(btnNewButton_12);
+        
+        JButton btnNewButton_13 = new JButton();
+        panel_2.add(btnNewButton_13);
 
         this.setVisible(true);
     }
-
-    public void flip(){}
 
     public static void main(String[] args) {
         new Memorice();
